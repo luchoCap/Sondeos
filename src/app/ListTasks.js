698ef1b0ab4f4e09e375ba59a5c4fc39cdc5bf28
luchoCap <luciano.capraro@outlook.com>
2020-02-10 00:10:09 -0300

    se cambio un component class por un functional

--- src/app/ListTasks.js
+++ src/app/ListTasks.js
@@ -1,47 +1,46 @@
 import React, { Component } from 'react';
 
 
-class ListTasks extends Component {
+function ListTasks(props) {
 
 
 
-    render() {
-        return (
-            <div className="card" >
-                <h4 style={{ fontFamily: 'cursive', fontSize: '2em', textAlign: 'center', paddingTop: '40px' }}>Tareas</h4>
-                <div className="card-content">
+    return (
+        <div className="card" >
+            <h4 style={{ fontFamily: 'cursive', fontSize: '2em', textAlign: 'center', paddingTop: '40px' }}>Tareas</h4>
+            <div className="card-content">
 
-                    <table className="striped">
-                        <thead>
-                            <tr>
-                                <th style={{ fontFamily: 'auto', fontSize: '1.2em' }}> Descripción Tareas</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.props.tasks.map(task => {
-                                    return (
-                                        <tr key={task._id}>
-                                            <td>
-                                                {task.title}
-                                            </td>
+                <table className="striped">
+                    <thead>
+                        <tr>
+                            <th style={{ fontFamily: 'auto', fontSize: '1.2em' }}> Descripción Tarea</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            props.tasks.map(task => {
+                                return (
+                                    <tr key={task._id}>
+                                        <td>
+                                            {task.title}
+                                        </td>
 
-                                            <td style={{ textAlign: 'end' }}>
-                                                <button className="btn light-blue darken-4" onClick={() => this.props.deleteTask(task._id)}><i className="material-icons">delete</i></button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+                                        <td style={{ textAlign: 'end' }}>
+                                            <button className="btn light-blue darken-4" onClick={() => props.deleteTask(task._id)}><i className="material-icons">delete</i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
 
 
-                </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 
 }
 
